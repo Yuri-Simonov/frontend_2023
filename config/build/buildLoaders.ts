e7e6b043cfd940d3e8f8c26145c5f54f741e0d69
Yuri Simonov <yura15-96@mail.ru>
2023-05-18 18:14:36 +0300

    4. Подключаем React и настраиваем scss в webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,25 @@
 import webpack from "webpack";
 
+// правила обработки файлов, выходящих за рамки JS
 export function buildLoaders(): webpack.RuleSetRule[] {
+	// Если не используется TS, нужен "babel-loader" для преобразования нового стандарта JS в старый, понятный многим браузерам
 	const typescriptLoader = {
 		test: /\.tsx?$/,
 		use: "ts-loader",
 		exclude: /node_modules/,
 	};
 
-	return [typescriptLoader];
+	const scssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			// Creates `style` nodes from JS strings
+			"style-loader",
+			// Translates CSS into CommonJS
+			"css-loader",
+			// Compiles Sass to CSS
+			"sass-loader",
+		],
+	};
+
+	return [typescriptLoader, scssLoader];
 }
